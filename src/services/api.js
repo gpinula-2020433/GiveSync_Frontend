c2c044a2d1926e191808506357c0fd27f5d64a2a
kterraza-2023022 <kterraza-2023022@kinal.edu.gt>
2025-06-19 14:30:23 -0600

    se agrega condicional en comentarios

--- src/services/api.js
+++ src/services/api.js
@@ -54,7 +54,7 @@ apiInstitucion.interceptors.request.use(
 
 apiComment.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) config.headers.Authorization = token
   return config
 })
 
