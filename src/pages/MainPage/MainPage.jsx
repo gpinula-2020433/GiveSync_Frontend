6d57f49ed49764082818e83d35df0c4b60196955
gpinula-2020433 <gpinula-2020433@kinal.edu.gt>
2025-07-06 00:29:24 -0600

    Se sube logo con contorno blanco y el normal por si acaso, se arregla el navbar

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,22 @@
+// src/pages/Main/MainPage.jsx
 import React from "react";
-import Layout from "../../components/Layout/Layout";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet } from "react-router-dom";
+import Navbar from '../../components/layout/navbar/Navbar';
+import Footer from '../../components/layout/footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const MainPage=() => {
-    return (
-      <Layout>
-        <Outlet />
-      </Layout>
-    );
-}
+const MainPage = () => {
+  return (
+    <div className="main-page-container">
+      <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
+        <Navbar />
+        <div className="flex-grow-1" style={{ marginTop: '60px' }}>
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
