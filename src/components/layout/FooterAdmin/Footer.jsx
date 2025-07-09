aa33fa8f7ed54039694ba0f27f48c97d5b2825ec
gpinula-2020433 <gpinula-2020433@kinal.edu.gt>
2025-07-09 15:25:49 -0600

    Se arreglan problemas de estilo

--- src/components/layout/FooterAdmin/Footer.jsx
+++ src/components/layout/FooterAdmin/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light py-3 w-100 position-fixed bottom-0">
+    <footer className="bg-dark text-light py-3 w-100">
   <div className="container text-center">
     <p className="mb-0 text-center">Sintax Error</p>
   </div>
--- src/pages/UserSettingsPage/UserSettingsPage.jsx
+++ src/pages/UserSettingsPage/UserSettingsPage.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Layout/navbar/Navbar'
-import SidebarUserSettings from '../../components/layout/Sidevar/SidebarUserSettings'
+import SidebarUserSettings from '../../components/Layout/Sidevar/SidebarUserSettings'
 import Footer from '../../components/Layout/FooterAdmin/Footer'
- 
+
 export const UserSettingsPage = () => {
   return (
-    <div className="admin-page-container">
-    <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-            <Navbar />
-      <div className="d-flex" style={{ marginTop: '60px' }}>
+    <div className="admin-page-container d-flex flex-column" style={{ minHeight: '100vh' }}>
+      <Navbar />
+      <div className="d-flex flex-grow-1" style={{ marginTop: '60px' }}>
         <SidebarUserSettings />
         <main className="flex-grow-1 p-4">
           <Outlet />
         </main>
       </div>
       <Footer />
-    </div>
     </div>
   )
 }
